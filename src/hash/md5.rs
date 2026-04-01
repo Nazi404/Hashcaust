306f09a7f9db9c18336021b582c5820f7ac19a0c
Anon404 <ninjab467@gmail.com>
2026-04-01 05:29:54 +0000

    resolved conflict

--- src/hash/md5.rs
+++ src/hash/md5.rs
@@ -1,7 +1,6 @@
 use md5;
 
 pub fn crack_md5(hash: &str, word: &str) -> Option<String> {
-    println!("{:?}", word);
     let digest = format!("{:x}", md5::compute(word));
     if hash == digest {
         Some(word.to_string())
--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ fn main() {
                 .long("increment")
                 .value_name("INCREMENTAL VALUE")
                 .help("brute force attack")
-                .required_if_eq("mode", "increment")
+                .required_if_eq("mode", "brute"),
         )
         .arg(
             Arg::new("type")
@@ -62,62 +62,71 @@ fn main() {
     println!("{}\n", BANNER.bright_green().bold().to_string());
 
     let hash = match_cli.get_one::<String>("hash").unwrap();
-    let wordlst = match_cli.get_one::<String>("wordlist").unwrap();
-    let increment = match_cli.get_one::<String>("increment").unwrap();
+    let wordlst = match_cli.get_one::<String>("wordlist");
+    let increment = match_cli.get_one::<String>("increment");
     let type_hash = match_cli.get_one::<String>("type").unwrap();
-    let mode = match_cli.get_one::<String>("mode").unwrap();
-
+    let mode = match_cli.get_one::<String>("mode").unwrap().as_str();
 
     println!("Hash: {}", hash);
-    println!("Wordlist: {}", wordlst);
-    println!("Type hash: {}", type_hash);
+    println!("Type: {}", type_hash);
     println!("Mode: {}", mode);
 
-    match mode.as_str() {
+    if mode == "wordlist" {
+        let w = wordlst.expect("Wordlist is required for wordlist mode");
+        println!("wordlist: {}", w);
+    } else if mode == "brute" {
+        let i = increment.expect("Mask is required for brute mode");
+        println!("mask: {}", i);
+    } else {}
 
-        "wordlist" => match type_hash.as_str() {
+    match mode {
+        "wordlist" => {
+            let wordlst = wordlst.expect("Wordlist is required for wordlist mode");
+            match type_hash.as_str() {
+                "blake2b" => wordlist(crack_blake2b, wordlst.as_str(), hash.as_str()),
+                "blake2s" => wordlist(crack_blake2s, wordlst.as_str(), hash.as_str()),
+                "blake3" => wordlist(crack_blake3, wordlst.as_str(), hash.as_str()),
+                "md4" => wordlist(crack_md4, wordlst.as_str(), hash.as_str()),
+                "md5" => wordlist(crack_md5, wordlst.as_str(), hash.as_str()),
+                "sha1" => wordlist(crack_sha1, wordlst.as_str(), hash.as_str()),
+                "sha224" => wordlist(crack_sha224, wordlst.as_str(), hash.as_str()),
+                "sha256" => wordlist(crack_sha256, wordlst.as_str(), hash.as_str()),
+                "sha384" => wordlist(crack_sha384, wordlst.as_str(), hash.as_str()),
+                "sha512" => wordlist(crack_sha512, wordlst.as_str(), hash.as_str()),
+                "sha3_224" => wordlist(crack_sha3_224, wordlst.as_str(), hash.as_str()),
+                "sha3_256" => wordlist(crack_sha3_256, wordlst.as_str(), hash.as_str()),
+                "sha3_384" => wordlist(crack_sha3_384, wordlst.as_str(), hash.as_str()),
+                "sha3_512" => wordlist(crack_sha3_512, wordlst.as_str(), hash.as_str()),
 
-            "blake2b" => wordlist(crack_blake2b, wordlst.as_str(), hash.as_str()),
-            "blake2s" => wordlist(crack_blake2s, wordlst.as_str(), hash.as_str()),
-            "blake3" => wordlist(crack_blake3, wordlst.as_str(), hash.as_str()),
-            "md4" => wordlist(crack_md4, wordlst.as_str(), hash.as_str()),
-            "md5" => wordlist(crack_md5, wordlst.as_str(), hash.as_str()),
-            "sha1" => wordlist(crack_sha1, wordlst.as_str(), hash.as_str()),
-            "sha224" => wordlist(crack_sha224, wordlst.as_str(), hash.as_str()),
-            "sha256" => wordlist(crack_sha256, wordlst.as_str(), hash.as_str()),
-            "sha384" => wordlist(crack_sha384, wordlst.as_str(), hash.as_str()),
-            "sha512" => wordlist(crack_sha512, wordlst.as_str(), hash.as_str()),
-            "sha3_224" => wordlist(crack_sha3_224, wordlst.as_str(), hash.as_str()),
-            "sha3_256" => wordlist(crack_sha3_256, wordlst.as_str(), hash.as_str()),
-            "sha3_384" => wordlist(crack_sha3_384, wordlst.as_str(), hash.as_str()),
-            "sha3_512" => wordlist(crack_sha3_512, wordlst.as_str(), hash.as_str()),
-            
-            _ => {
-                eprintln!("Hash type not supported or invalid type");
-                return;
+                _ => {
+                    eprintln!("Hash type not supported or invalid type");
+                    return;
+                }
             }
-        },
-        "brute" => match type_hash.as_str() {
-
-            "blake2b" => brute_mask(crack_blake2b, increment.as_str(), hash.as_str()),
-            "blake2s" => brute_mask(crack_blake2s, increment.as_str(), hash.as_str()),
-            "blake3" => brute_mask(crack_blake3, increment.as_str(), hash.as_str()),
-            "md4" => brute_mask(crack_md4, increment.as_str(), hash.as_str()),
-            "md5" => brute_mask(crack_md5, increment.as_str(), hash.as_str()),
-            "sha1" => brute_mask(crack_sha1, increment.as_str(), hash.as_str()),
-            "sha224" => brute_mask(crack_sha224, increment.as_str(), hash.as_str()),
-            "sha256" => brute_mask(crack_sha256, increment.str(), hash.as_str()),
-            "sha384" => brute_mask(crack_sha384, increment.as_str(), hash.as_str()),
-            "sha512" => brute_mask(crack_sha512, increment.as_str(), hash.as_str()),
-            "sha3_224" => brute_mask(crack_sha3_224, increment.as_str(), hash.as_str()),
-            "sha3_256" => brute_mask(crack_sha3_256, increment.as_str(), hash.as_str()),
-            "sha3_384" => brute_mask(crack_sha3_384, increment.as_str(), hash.as_str()),
-            "sha3_512" => brute_mask(crack_sha3_512, increment.as_str(), hash.as_str()),
-            _ => {
-                eprintln!("Hash type not supported or invalid type");
-                return;
+        }
+        "brute" => {
+            let increment = increment.expect("Mask is required for brute mode");
+            match type_hash.as_str() {
+                "blake2b" => brute_mask(crack_blake2b, increment.as_str(), hash.as_str()),
+                "blake2s" => brute_mask(crack_blake2s, increment.as_str(), hash.as_str()),
+                "blake3" => brute_mask(crack_blake3, increment.as_str(), hash.as_str()),
+                "md4" => brute_mask(crack_md4, increment.as_str(), hash.as_str()),
+                "md5" => brute_mask(crack_md5, increment.as_str(), hash.as_str()),
+                "sha1" => brute_mask(crack_sha1, increment.as_str(), hash.as_str()),
+                "sha224" => brute_mask(crack_sha224, increment.as_str(), hash.as_str()),
+                "sha256" => brute_mask(crack_sha256, increment.as_str(), hash.as_str()),
+                "sha384" => brute_mask(crack_sha384, increment.as_str(), hash.as_str()),
+                "sha512" => brute_mask(crack_sha512, increment.as_str(), hash.as_str()),
+                "sha3_224" => brute_mask(crack_sha3_224, increment.as_str(), hash.as_str()),
+                "sha3_256" => brute_mask(crack_sha3_256, increment.as_str(), hash.as_str()),
+                "sha3_384" => brute_mask(crack_sha3_384, increment.as_str(), hash.as_str()),
+                "sha3_512" => brute_mask(crack_sha3_512, increment.as_str(), hash.as_str()),
+                _ => {
+                    eprintln!("Hash type not supported or invalid type");
+                    return;
+                }
             }
-        },
+        }
         _ => {
             eprintln!("❌ Invalid mode!");
             return;
