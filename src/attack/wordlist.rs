04477828f9975f71227a348003649c63bb3db0b1
Anon404 <ninjab467@gmail.com>
2026-04-11 05:04:56 +0000

    lua rules

--- src/attack/wordlist.rs
+++ src/attack/wordlist.rs
@@ -11,34 +11,24 @@ pub fn wordlist(
     let file = File::open(path).expect("Unable to open file");
     let reader = BufReader::new(file);
 
+    let lua = Lua::new();
+    let mut rulefunc:Option<mlua::Function> = None;
     if let Some(rule) = rule {
 
-        let lua = Lua::new();
+        // lua = Lua::new();
         let code = read_to_string("src/rules/rules.lua").unwrap();
         lua.load(&code).exec().unwrap();
         let globals = lua.globals();
 
-        let rulefunc:mlua::Function = match rule.as_str() {
-            "uppercase" => globals.get("uppercase").unwrap();
-            "lowercase" => globals.get("lowercase").unwrap();
-            "togglecase" => globals.get("togglecase").unwrap();
-            "capitalize" => globals.get("capitalize").unwrap();
-            "reverse" => globals.get("reverse").unwrap();
-            "leet" => globals.get("leet").unwrap();
-            _ => {
-                eprintln!("Invalid rule");
-                return;
-            }
-        }
+        rulefunc = Some(globals.get(rule.as_str()).expect("Unknown rule"));
     }
-    else {}
 
     for line in reader.lines() {
 
         if let Ok(mut line) = line {
 
             if let Some(func) = &rulefunc {
-                line = func.call((line)).unwrap();
+                line = func.call(line).unwrap();
             }
 
             if let Some(result) = f(hash, &line) {
